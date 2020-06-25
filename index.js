a5bec9af1e67a0d8e55f5bae02b8b511419b7e84
Davidoffili <palagashvilid@gmail.com>
2020-06-24 20:45:43 -0700

    switch branches

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 // boilerplate from https://www.npmjs.com/package/json-server
-const jsonServer = require('json-server')
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults()
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
 // const PORT = process.env.PORT || 3001;
 
-server.use(middlewares)
-server.use(router)
+server.use(middlewares);
+server.use(router);
 
 //Routes
 server.get('/categories', (request, response) => {
     const url = 'http://localhost:3000/categories'
-})
+});
 
 server.listen(3000, () => {
-  console.log(`JSON Server is running`)
+    console.log(`JSON Server is running`)
 });
