c595980b6fe0b6c89e25406da5a2f894a5fec298
Davidoffili <palagashvilid@gmail.com>
2020-06-25 20:15:51 -0700

    finished 404 error handler module

--- middleware/404.js
+++ middleware/404.js
@@ -1,12 +1,9 @@
 'use strict';
+// Snuck back into my 301 work for a refresher on error handling --> https://github.com/Davidoffili/city_explorer_api/blob/master/modules/errors.js
 
-/* TODO: 404.js module, tasks:
-
- - Sends a 404/Not-Found message as the response (does not call .next())
- - Import this into your server and set it up to be “used” after your other routes
-
-*/
+// This module sends a 404/Not-Found message as the response.
 
 module.exports = (request, response, error) => {
-    console.log('__ERROR__')
-}
+    console.log('__ERROR__: ', error); // this will log and show error details
+    response.status(404).send('We cannot ' + `${request.method}` + `${request.path}`); // Sends a 404 message.
+}; // exported to server and set up to be “used” after our other routes
--- middleware/logger.js
+++ middleware/logger.js
@@ -1,7 +1,7 @@
 'use strict';
 // https://stackify.com/javascript-logging-basic-tips/
 
-// This module executes a console.log() containing the request path, method, and the requestTime property of the request object
+// This module executes a console.log() containing the request path, method, and the requestTime property of the request object.
 
 module.exports = (request, response, next) => {
     console.log('__REQUEST__: ' + `${request.path}` + `${request.method}` + `${request.requestTime}`);
