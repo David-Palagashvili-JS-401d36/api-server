b995f4baed305295a799de6e598d00bcf23d49b9
Davidoffili <palagashvilid@gmail.com>
2020-06-27 16:00:03 -0700

    built out file structure for Phase 3 modularization

---
+++ routes/products.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+
+/* TODO: Create a router module for the category data type:
+
+- Create an instance of express.Router() and export it
+- Move the appropriate route definitions and route handlers from server.js here
+- Redefine the definitions as route definitions (not app definitions)
+- Confirm that your server works as before, but now modular
+- Begin the conversion from memory data to persistent data …
+- Import and initialize the appropriate Mongoose Collection (see bullet points below)
+- In your handler methods, rather than change your in-memory data store, call the appropriate model methods
+
+*/
