e86f019b1a61488a34ac3693dbc6fc9f8cfdc1ff
Davidoffili <palagashvilid@gmail.com>
2020-06-26 20:08:56 -0700

    started writing test for some of the CRUD operations in server.js

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -1,7 +1,14 @@
 'use strict';
+// NOTE to TA: I relied heavily on the jest docs, no way I could retain everything from the lecture. line 17 was something I found halfway down the page --> https://jestjs.io/docs/en/jest-object
 
-/* TODO: Write unit tests for the middleware, these should be testable in isolation:
+// require testing tools
+const superGoose = require('cf-supergoose');
+const server = require('../lib/server.js');
 
- - Use supergoose or supertest to easily test your server without having to start it
+// assign a variable to expect an express server. Allows us to easily test our server without having to start it.
+const mockRoute = superGoose.server(server.server);
 
-*/
+// create a mock function that tracks calls to console[log]. The chained method allows us to overwrite the original function.
+jest.spyOn(console, 'log').mockImplementation();
+
+// test each CRUD operation.
