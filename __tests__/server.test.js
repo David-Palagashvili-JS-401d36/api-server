c5205aa3d3d9af48ed4a110a706e7c79e1874cf5
Davidoffili <palagashvilid@gmail.com>
2020-06-26 21:24:10 -0700

    wrote tests for GET and POST routes for products

--- __tests__/server.test.js
+++ __tests__/server.test.js
@@ -11,4 +11,23 @@ const mockRoute = superGoose.server(server.server);
 // create a mock function that tracks calls to console[log]. The chained method allows us to overwrite the original function.
 jest.spyOn(console, 'log').mockImplementation();
 
-// test each CRUD operation.
+// test each CRUD operation:
+
+describe('testing api-server', () => {
+    // test for GET route
+    it('should be able to respond to the GET route for products', () => {
+        return mockRoute.get('/products')
+        .then(results => {
+            expect(results.body.length).toBe(0);
+        });
+    });
+    // test for POST route
+    it('should be able to respond to the POST route for products', () => {
+        const sampleProduct = {id: 2, name: 'product 2'};
+        return mockRoute.get('/products')
+        .send(sampleProduct)
+        .then(results => {
+            expect(results.body).toStrictEqual(sampleProduct);
+        });
+    });
+});
