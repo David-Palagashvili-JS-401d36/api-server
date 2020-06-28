4cc5e3dfed7febc2a0b5d06a2cc06dc1cb964102
Davidoffili <palagashvilid@gmail.com>
2020-06-27 20:02:41 -0700

    opened a default mongoose connection

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // NOTE: this file is “entry point” to our server:
 
 // TODO: Update the index:
-// Require the mongoose library.
+
 // Prior to calling start() on your imported server module, it connect to Mongo, via mongoose.
 
 // require dotenv and the mongoose library.
@@ -14,6 +14,14 @@ const PORT = process.env.PORT || 3001;
 
 // require lib/server.js
 const expServer = require('./lib/server.js');
+
+// connect mongo database, open a default mongoose connection
+mongoose.connect(process.env.MONGODB_ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+
 
 // call the .start() method from the server with the PORT set in your environment
 expServer.start(PORT);
