324085d05c1d083fa11aa2828f44018e688a8f49
Davidoffili <palagashvilid@gmail.com>
2020-06-28 13:53:29 -0700

    error handlers misbehaving, modularizing routes, then see where it's choking up

--- routes/categories.js
+++ routes/categories.js
@@ -2,10 +2,22 @@
 
 const express = require('express');
 
+// Create an instance of express.Router() and export it
+const router = express.Router();
+
+// Require our category model
+const CategoryModel = require('../lib/models/categories/categories.collection.js');
+// instantiate the model
+const Category = new CategoryModel();
+
+// Appropriate route definitions and route handlers from server.js, moved here:
+router.post('/', addCategory);
+router.get('/', getAllCategories);
+router.get('/:id', getCategory);
+router.put('/:id', updateCategory);
+router.delete('/:id', deleteCategory);
 /* TODO: Create a router module for the category data type:
 
-- Create an instance of express.Router() and export it
-- Move the appropriate route definitions and route handlers from server.js here
 - Redefine the definitions as route definitions (not app definitions)
 - Confirm that your server works as before, but now modular
 - Begin the conversion from memory data to persistent data …
