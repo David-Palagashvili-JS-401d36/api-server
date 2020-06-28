e696564904ebf7903f37793431428571c6f2652d
Davidoffili <palagashvilid@gmail.com>
2020-06-28 15:25:06 -0700

    fixed error middleware

--- routes/products.js
+++ routes/products.js
@@ -1,19 +1,5 @@
 'use strict';
-
 const express = require('express');
-
-/* TODO: Create a router module for the Product data type:
-
-- Create an instance of express.Router() and export it
-- Move the appropriate route definitions and route handlers from server.js here
-- Redefine the definitions as route definitions (not app definitions)
-- Confirm that your server works as before, but now modular
-- Begin the conversion from memory data to persistent data …
-- Import and initialize the appropriate Mongoose Collection (see bullet points below)
-- In your handler methods, rather than change your in-memory data store, call the appropriate model methods
-
-I have a feeling this will be very similar to how we did products
-*/
 
 // Create an instance of express.Router()
 const router = express.Router();
